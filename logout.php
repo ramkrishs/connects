61f83e68dfa5d54a42c83d4d1c9285b276750c42
Ramakrishnan Sathyavageeswaran <ramkrishs@outlook.com>
2014-01-27 00:48:11 +0530

    c

--- logout.php
+++ logout.php
@@ -7,12 +7,12 @@ $dbc = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
 mysqli_query($dbc,"UPDATE `questions` SET `status`=0 WHERE `q_id`!=999");
 
 $save_value=$_POST['save'];
-if ($save_value==1) {
+if ($save_value=="1" && isset($save_value)) {
 	$user_empid=$_SESSION['empid'];
 	$user_name=$_SESSION['name'];
 	$user_score=$_SESSION['mark'];
 	//insert query
-	
+	mysqli_query($dbc,"INSERT INTO `results`(`empid`, `name`, `score`) VALUES ('$user_empid','$user_name','$user_score')");
 }
 // Delete certain session
 // unset($_SESSION['descrip']);
